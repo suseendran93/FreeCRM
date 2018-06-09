b20cb784e746028df7c800efda21320b500d4cef
suseendran93 <ssndrn@gmail.com>
2018-06-09 04:10:24 -0700

    Test base code updated

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -1,5 +1,59 @@
 package com.crm.qa.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class TestBase {
 
+	static WebDriver driver;
+	static Properties prop;
+
+	public TestBase() {
+
+		// To read from config.property file
+
+		try {
+
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream("C:\\Users\\ssndr\\eclipse-workspace\\"
+					+ "FreeCRMTest\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void intialization() {
+		String browserName=prop.getProperty("browser");
+		
+		if(browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\ssndr\\Documents\\Drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			
+		}
+		else if(browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\ssndr\\Documents\\Drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+	}
+	
+	
+	
+	
 }
